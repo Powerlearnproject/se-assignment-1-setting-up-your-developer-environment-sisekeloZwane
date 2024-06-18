38ae055bb9573e451fa08edd763edd993a748782
Sisekelo <sisekelozwane13@gmail.com>
2024-06-18 23:46:00 +0200

    the readme and first main python

---
+++ main.py
@@ -0,0 +1,14 @@
+def add(a, b):
+    Function to add two integers.
+    return a + b
+
+def main():
+    # Define two integers
+    num1 = 5
+    num2 = 7
+    
+    # Add the two integers
+    result = add(num1, num2)
+    
+    # Print the result
+    print(result) 
